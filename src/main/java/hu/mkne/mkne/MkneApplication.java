98477326c065690ae88d07994be352a55c7e2ca1
mkmdev <mkmdev18@gmail.com>
2019-09-11 10:10:36 +0200

    Initial commit

---
+++ src/main/java/hu/mkne/mkne/MkneApplication.java
@@ -0,0 +1,13 @@
+package hu.mkne.mkne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkneApplication.class, args);
+    }
+
+}
