850ef2cb9e45d3f1fbe1b8fd92314c442914871e
mkmdev <mkmdev18@gmail.com>
2019-09-11 13:38:02 +0200

    Implemented basic MKNE main entry point with initialization method for a dummy post and user.

--- src/main/java/hu/mkne/mkne/MkneApplication.java
+++ src/main/java/hu/mkne/mkne/MkneApplication.java
@@ -1,13 +1,47 @@
 package hu.mkne.mkne;
 
+import hu.mkne.mkne.model.Post;
+import hu.mkne.mkne.model.PostCategory;
+import hu.mkne.mkne.model.Member;
+import hu.mkne.mkne.repository.MemberRepository;
+import hu.mkne.mkne.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class MkneApplication {
+
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(MkneApplication.class, args);
     }
 
+    @Bean
+    @Profile("production")
+    public CommandLineRunner init() {
+        return args -> {
+            Member member1 = Member.builder()
+                    .email("test@test.com")
+                    .username("test")
+                    .password("1234")
+                    .build();
+
+            Post post1 = Post.builder()
+                    .category(PostCategory.NEWS)
+                    .postBody("This is a test post. Dolorem ipsum sit dolor amet.")
+                    .title("First test")
+                    .author(member1)
+                    .build();
+
+            memberRepository.save(member1);
+            postRepository.save(post1);
+        };
+    }
 }
