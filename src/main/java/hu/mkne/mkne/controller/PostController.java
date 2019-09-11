4bee38252979ccc5de54930976001ebe21df0ed2
mkmdev <mkmdev18@gmail.com>
2019-09-11 13:39:32 +0200

    + get all posts ordered by publication date via post service

---
+++ src/main/java/hu/mkne/mkne/controller/PostController.java
@@ -0,0 +1,25 @@
+package hu.mkne.mkne.controller;
+
+import hu.mkne.mkne.model.Post;
+import hu.mkne.mkne.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+@CrossOrigin
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService service;
+
+    @GetMapping
+    public List<Post> getPosts() {
+        return service.getOrderedPosts();
+    }
+}
