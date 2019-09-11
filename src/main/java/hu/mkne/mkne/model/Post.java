b0c14df820b93b123cf424839294b63665efefc3
mkmdev <mkmdev18@gmail.com>
2019-09-11 14:13:26 +0200

    + publish test post and update

--- src/main/java/hu/mkne/mkne/model/Post.java
+++ src/main/java/hu/mkne/mkne/model/Post.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -25,9 +25,11 @@ public class Post {
     private Member author;
 
     @CreationTimestamp
-    private Timestamp creationDate;
+    private LocalDateTime creationDate;
 
-    private Timestamp publishDate;
+    private LocalDateTime publishDate;
+
+    private Boolean isPublished;
 
     private String title;
 
@@ -36,6 +38,12 @@ public class Post {
     @Enumerated(value = EnumType.STRING)
     @Column(nullable = false)
     private PostCategory category;
+
+
+    public void publishPost() {
+        this.isPublished = true;
+        setPublishDate(LocalDateTime.now());
+    }
 
     @Override
     public String toString() {
@@ -49,4 +57,5 @@ public class Post {
                 ", category=" + category +
                 '}';
     }
+
 }
