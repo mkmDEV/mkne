1fb4cec51c39a0bee9978fc12cf1af0dae1ab58c
mkmdev <mkmdev18@gmail.com>
2019-09-12 10:21:02 +0200

    fixed an issue, when unpublished post doesn't get false value on creation added more dummy data

--- src/main/java/hu/mkne/mkne/MkneApplication.java
+++ src/main/java/hu/mkne/mkne/MkneApplication.java
@@ -40,12 +40,34 @@ public class MkneApplication {
                     .postBody("This is a test post. Dolorem ipsum sit dolor amet.")
                     .title("First test")
                     .author(member1)
+                    .isPublished(false)
                     .build();
+
+            Post unpublishedPost = Post.builder()
+                    .category(PostCategory.NEWS)
+                    .postBody("This post never published. Only for testing.")
+                    .title("Unpublished post test")
+                    .author(member1)
+                    .isPublished(false)
+                    .build();
+
+            Post advert = Post.builder()
+                    .category(PostCategory.ADVERT)
+                    .postBody("This is a very new fancy advertistment.")
+                    .title("Veddmegmost!")
+                    .author(member1)
+                    .isPublished(false)
+                    .build();
+
 
             memberRepository.save(member1);
             postRepository.save(post1);
+            postRepository.save(unpublishedPost);
+            postRepository.save(advert);
             post1.publishPost();
+            advert.publishPost();
             postRepository.saveAndFlush(post1);
+            postRepository.saveAndFlush(advert);
         };
     }
 }
