ebf0b15e339ad4f465debce797d978ac560a0c7e
mkmdev <mkmdev18@gmail.com>
2019-09-12 10:22:38 +0200

    Show only published ads, news

--- src/main/java/hu/mkne/mkne/repository/PostRepository.java
+++ src/main/java/hu/mkne/mkne/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package hu.mkne.mkne.repository;
 
 import hu.mkne.mkne.model.Post;
+import hu.mkne.mkne.model.PostCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,8 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> getAllByOrderByPublishDateDesc();
+
+    List<Post> getPostsByCategoryAndIsPublishedTrueOrderByPublishDateDesc(PostCategory category);
+
+    List<Post> getPostsByIsPublishedTrueOrderByPublishDateDesc();
 }
--- src/main/java/hu/mkne/mkne/service/PostService.java
+++ src/main/java/hu/mkne/mkne/service/PostService.java
@@ -1,6 +1,7 @@
 package hu.mkne.mkne.service;
 
 import hu.mkne.mkne.model.Post;
+import hu.mkne.mkne.model.PostCategory;
 import hu.mkne.mkne.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,17 @@ public class PostService {
 
     public List<Post> getOrderedPosts() {
         return repository.getAllByOrderByPublishDateDesc();
+    }
+
+    public List<Post> getAds() {
+        return repository.getPostsByCategoryAndIsPublishedTrueOrderByPublishDateDesc(PostCategory.ADVERT);
+    }
+
+    public List<Post> getAllPublishedPosts() {
+        return repository.getPostsByIsPublishedTrueOrderByPublishDateDesc();
+    }
+
+    public List<Post> getAllPublishedNews() {
+        return repository.getPostsByCategoryAndIsPublishedTrueOrderByPublishDateDesc(PostCategory.NEWS);
     }
 }
