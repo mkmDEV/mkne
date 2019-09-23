70c3cb5c777784dd713061d05b31a4bb24f8d98f
mkmdev <mkmdev18@gmail.com>
2019-09-23 19:21:05 +0200

    + swagger

--- src/main/java/hu/mkne/mkne/MkneApplication.java
+++ src/main/java/hu/mkne/mkne/MkneApplication.java
@@ -11,9 +11,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@EnableSwagger2
 public class MkneApplication {
 
     private final MemberRepository memberRepository;
@@ -21,6 +27,15 @@ public class MkneApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MkneApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
     @Bean
