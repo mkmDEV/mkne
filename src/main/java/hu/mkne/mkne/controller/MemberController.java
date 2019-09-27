e92e71dca1bd9135ae649a3cd66cb611a9034422
mkmdev <mkmdev18@gmail.com>
2019-09-27 12:52:13 +0200

    + cross origin

--- src/main/java/hu/mkne/mkne/controller/MemberController.java
+++ src/main/java/hu/mkne/mkne/controller/MemberController.java
@@ -3,12 +3,14 @@ package hu.mkne.mkne.controller;
 import hu.mkne.mkne.model.Member;
 import hu.mkne.mkne.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 @RequiredArgsConstructor
 public class MemberController {
 
