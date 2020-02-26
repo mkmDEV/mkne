81284f9a00d60ae15568de58ed2278899287c751
mkmdev <mkmdev18@gmail.com>
2020-02-26 13:48:11 +0100

    + jwt filter

---
+++ src/main/java/hu/mkne/mkne/security/JwtTokenFilter.java
@@ -0,0 +1,31 @@
+package hu.mkne.mkne.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtTokenFilter extends GenericFilterBean {
+
+    private JwtTokenService jwtTokenService;
+
+    JwtTokenFilter(JwtTokenService jwtTokenService) {
+        this.jwtTokenService = jwtTokenService;
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException {
+        String token = jwtTokenService.getTokenFromRequest((HttpServletRequest) req);
+        if (token != null && jwtTokenService.validateToken(token)) {
+            Authentication auth = jwtTokenService.parseUserFromTokenInfo(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(req, res);
+    }
+}
