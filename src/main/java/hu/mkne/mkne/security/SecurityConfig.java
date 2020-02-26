b6e04727b4441d596b52d9286b328f44fd5e31af
mkmdev <mkmdev18@gmail.com>
2020-02-26 13:35:12 +0100

    extended WSCM, wired jwts

---
+++ src/main/java/hu/mkne/mkne/security/SecurityConfig.java
@@ -0,0 +1,14 @@
+package hu.mkne.mkne.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenService jwtTokenService;
+
+    public SecurityConfig(JwtTokenService jwtTokenService) {
+        this.jwtTokenService = jwtTokenService;
+    }
+}
