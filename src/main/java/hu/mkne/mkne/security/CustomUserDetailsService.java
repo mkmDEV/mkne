a956e1d508839a264c9b3f6adf0adc050cb12c28
mkmdev <mkmdev18@gmail.com>
2020-02-26 14:24:57 +0100

    + CustomUserDetailsService

--- src/main/java/hu/mkne/mkne/security/CustomUserDetailsService.java
+++ src/main/java/hu/mkne/mkne/security/CustomUserDetailsService.java
@@ -1,4 +1,33 @@
 package hu.mkne.mkne.security;
 
-public class CustomUserDetailsService {
+import hu.mkne.mkne.model.Member;
+import hu.mkne.mkne.repository.MemberRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private MemberRepository users;
+
+    public CustomUserDetailsService(MemberRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member user = users.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Email: %s not found", email)));
+
+        return new User(user.getEmail(), user.getPassword(), user.getRoles()
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList()));
+    }
 }
