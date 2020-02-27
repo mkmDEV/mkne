6028910c38e7eb1eafcb97fbc8f7ce3402a67c57
mkmdev <mkmdev18@gmail.com>
2020-02-27 11:30:06 +0100

    Registration endpoint works

--- src/main/java/hu/mkne/mkne/controller/RegistrationController.java
+++ src/main/java/hu/mkne/mkne/controller/RegistrationController.java
@@ -1,4 +1,22 @@
 package hu.mkne.mkne.controller;
 
+import hu.mkne.mkne.model.Member;
+import hu.mkne.mkne.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
 public class RegistrationController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/register")
+    public Member register(@RequestBody Member member) {
+        return memberService.register(member);
+    }
 }
